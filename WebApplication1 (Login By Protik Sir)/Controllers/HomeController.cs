88654c66ce671a24f863e025f94f7cc20ada957c
nafirk99 <nafiz.ba.pakizatvl@gmail.com>
2024-02-28 12:36:43 +0600

    aaaa

--- WebApplication1 (Login By Protik Sir)/Controllers/HomeController.cs
+++ WebApplication1 (Login By Protik Sir)/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
