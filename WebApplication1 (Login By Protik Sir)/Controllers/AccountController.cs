6afa14389e32b86bc34d8efa5ceb02f64b4cbb57
nafirk99 <nafiz.ba.pakizatvl@gmail.com>
2024-03-07 18:36:26 +0600

    6

--- WebApplication1 (Login By Protik Sir)/Controllers/AccountController.cs
+++ WebApplication1 (Login By Protik Sir)/Controllers/AccountController.cs
@@ -13,5 +13,36 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Login(string btnSubmit, string txtUserName, string txtPassword)
+        {
+            string LoginMsg = "";
+            if (btnSubmit == "LogIn")
+            {
+                if(txtUserName == "Nafi" && txtPassword == "123456")
+                {
+                    Session["User"] = "Nafi";
+                    return RedirectToAction("Dashboard", "Home");
+                }
+                else
+                {
+                    LoginMsg = "Failed, UserName/Password Did Not Match";
+                }
+                 
+            }
+            else if (btnSubmit == "Forget Password")
+            {
+                return RedirectToAction("forget", "Account");
+            }
+            ViewBag.LoginMsg = LoginMsg;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            Session["User"] = null;
+            return RedirectToAction("Login", "Account");
+        }
+       
     }
 }
